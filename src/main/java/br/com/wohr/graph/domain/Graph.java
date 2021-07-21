b1d62c2e09f2ef9965765650c4240641e677247b
Willian-Mohr <willianbmohr@gmail.com>
2021-07-21 19:40:43 -0300

    Creating repository class for Data and Graph

--- src/main/java/br/com/wohr/graph/domain/Graph.java
+++ src/main/java/br/com/wohr/graph/domain/Graph.java
@@ -2,7 +2,6 @@ package br.com.wohr.graph.domain;
 
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
